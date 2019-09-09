40fdb8c5ec516173f2af0bec9e3d665932c0e3a7
Steven Eksteen <eksoverzero@outlook.com>
2019-09-09 09:42:49 +0200

    Standard result for saving

--- za-lottery/handler.js
+++ za-lottery/handler.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// const sqs = require('../lib/aws/sqs');
 const request = require('../lib/request/ithuba');
 
 module.exports.list = async (event, context) => {
@@ -10,7 +11,85 @@ module.exports.list = async (event, context) => {
 
 module.exports.get = async (event, context) => {
   const draw = context.draw;
-  const result = await request.singleResult(context.draw, 'LOTTO');
+  const response = await request.singleResult(context.draw, 'LOTTO');
+  const drawDetails = response.data.drawDetails;
 
-  console.log(result);
+  const payload = {
+    id: parseInt(drawDetails.drawNumber),
+    date: drawDetails.drawDate,
+    machine: drawDetails.drawMachine,
+    ball_set: drawDetails.ballSet,
+    results: [{
+      name: 'Ball 1',
+      value: parseInt(drawDetails.ball1),
+      position: 1
+    }, {
+      name: 'Ball 2',
+      value: parseInt(drawDetails.ball2),
+      position: 2
+    }, {
+      name: 'Ball 3',
+      value: parseInt(drawDetails.ball3),
+      position: 3
+    }, {
+      name: 'Ball 4',
+      value: parseInt(drawDetails.ball4),
+      position: 4
+    }, {
+      name: 'Ball 5',
+      value: parseInt(drawDetails.ball5),
+      position: 5
+    }, {
+      name: 'Ball 6',
+      value: parseInt(drawDetails.ball6),
+      position: 6
+    }, {
+      name: 'Bonus Ball',
+      value: parseInt(drawDetails.bonusBall),
+      position: 7
+    }],
+    prizes: [{
+      name: 'Match 6',
+      payout: parseFloat(drawDetails.div1Payout),
+      winners: parseInt(drawDetails.div1Winners),
+      position: 1
+    }, {
+      name: 'Match 5 + Bonus Ball',
+      payout: parseFloat(drawDetails.div2Payout),
+      winners: parseInt(drawDetails.div2Winners),
+      position: 2
+    }, {
+      name: 'Match 5',
+      payout: parseFloat(drawDetails.div3Payout),
+      winners: parseInt(drawDetails.div3Winners),
+      position: 3
+    }, {
+      name: 'Match 4 + Bonus Ball',
+      payout: parseFloat(drawDetails.div4Payout),
+      winners: parseInt(drawDetails.div4Winners),
+      position: 4
+    }, {
+      name: 'Match 4',
+      payout: parseFloat(drawDetails.div5Payout),
+      winners: parseInt(drawDetails.div5Winners),
+      position: 5
+    }, {
+      name: 'Match 3 + Bonus Ball',
+      payout: parseFloat(drawDetails.div6Payout),
+      winners: parseInt(drawDetails.div6Winners),
+      position: 6
+    }, {
+      name: 'Match 3',
+      payout: parseFloat(drawDetails.div7Payout),
+      winners: parseInt(drawDetails.div7Winners),
+      position: 7
+    }, {
+      name: 'Match 2 + Bonus Ball',
+      payout: parseFloat(drawDetails.div8Payout),
+      winners: parseInt(drawDetails.div8Winners),
+      position: 8
+    }]
+  };
+
+  console.log(payload);
 };
